0b94ef3619262f16ffd3ee01e5ccec1283d35380
Yann BECRET <ybecret@e3r5p21.42.fr>
2016-12-08 12:35:51 +0100

    adding ft_memmove fct

---
+++ ft_memmove.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/08 12:13:55 by ybecret           #+#    #+#             */
+/*   Updated: 2016/12/08 12:34:18 by ybecret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char *tmp;
+
+	tmp = dest;
+	if (src > dest)
+		dest = ft_memcpy(tmp, src, n);
+	if (src < dest)
+	{
+		src = src + (n - 1);
+		tmp = tmp + (n - 1);
+		while (n--)
+			*tmp-- = *(unsigned char*)src--;
+	}
+	return (dest);
+}
