27e2fe8a98331047e09a079b8b89fb5f1882d209
Yann BECRET <ybecret@e3r5p21.42.fr>
2016-12-08 12:06:33 +0100

    adding ft_memset fct

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 11:12:10 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/08 11:14:10 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/08 12:02:58 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void	*s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	char *tmp;
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/05 13:31:00 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/08 11:21:19 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/08 12:01:27 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,6 @@ void				ft_putnbr(int n);
 
 void				*ft_memset(void *s, int c, size_t n);
 void				ft_bzero(void *s, size_t n);
+void				*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
