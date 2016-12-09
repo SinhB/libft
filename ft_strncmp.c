0e34af9855354b241ad4283a02c71accba794ca6
Yann Becret <Yann@MacBook-Pro-de-Yann.local>
2016-12-09 17:33:59 +0100

    adding ft_strncmp fct

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,11 +3,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-  while (n)
+  while (n--)
     {
       if (ft_strcmp(s1, s2) != 0)
 	return (ft_strcmp(s1, s2));
-      n--;
+      s1++;
+      s2++;
     }
   return (0);
 }
