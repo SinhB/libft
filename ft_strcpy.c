5cacbeabfd8865d6648f232173e721e86c50070d
Yann Becret <Yann@MacBook-Pro-de-Yann.local>
2016-12-09 16:07:58 +0100

    adding ft_strcpy fct

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -12,16 +12,16 @@
 
 #include "libft.h"
 
-char	*ft_strcpy(char *dst, const char *src)
+char	*ft_strcpy(char *dest, const char *src)
 {
 	int i;
 
 	i = 0;
 	while (src[i])
 	{
-		dst[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
-	return (dst);
+	dest[i] = '\0';
+	return (dest);
 }
--- libft.h
+++ libft.h
@@ -41,7 +41,8 @@ int					ft_strnequ(char const *s1, char const *s2, size_t n);
 
 size_t				ft_strlen(const char *s);
 
-char				*ft_strcpy(char *dst, const char *src);
+char				*ft_strcpy(char *dest, const char *src);
+char				*ft_strncpy(char *dest, const char *src, size_t n);
 char				*ft_strdup(const char *s1);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strcat(char *dest, const char *src);
