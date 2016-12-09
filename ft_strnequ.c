1147dcd8d63479ff241ba1f01724f06c4c590b6a
Yann Becret <Yann@MacBook-Pro-de-Yann.local>
2016-12-09 15:19:13 +0100

    ft_strncmp SANS header

---
+++ ft_strnequ.c
@@ -0,0 +1,7 @@
+
+#include "libft.h"
+
+int	ft_strnequ(char const *s1, char const *s2, size_t n)
+{
+  return (ft_strncmp(s1, s2, n) != 0 ? 0 : 1);
+}
--- libft.h
+++ libft.h
@@ -37,6 +37,7 @@ int					ft_atoi(char *s);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_strequ(char const *s1, char const *s2);
+int					ft_strnequ(char const *s1, char const *s2, size_t n);
 
 size_t				ft_strlen(const char *s);
 
