ed21045d1a41e604421982415dc3fa090dc5e3ef
Yann Becret <Yann@MacBook-Pro-de-Yann.local>
2016-12-10 14:50:47 +0100

    ft_strmapi SANS header

---
+++ ft_strmapi.c
@@ -0,0 +1,19 @@
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+  char *new;
+  unsigned int i;
+
+  i = 0;
+  if(!(new = ft_strnew(ft_strlen(s))))
+    return (NULL);
+  while (s[i] != '\0')
+    {
+      new[i] = (*f)(i, s[i]);
+      i++;
+    }
+  new[i] = '\0';
+  return (new);
+}
