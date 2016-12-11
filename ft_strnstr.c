200bea5a4186bf3ce02b6893ebcef7eeb1ff8a0c
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 14:00:58 +0100

    adding ft_strnstr fct

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,25 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 13:59:15 by ybecret           #+#    #+#             */
+/*   Updated: 2016/12/11 14:00:30 by ybecret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-  int i;
+	int i;
 
-  i = 0;
-  if (!big)
-    {
-      if (*big == *little)
-	return (big);
-      else
+	i = 0;
+	if (!big)
+	{
+		if (*big == *little)
+			return (big);
+		else
+			return (NULL);
+	}
+	while (*big)
+	{
+		if (ft_strnequ(little, big, ft_strlen(little)))
+			if (ft_strlen(little) + i <= len)
+				return (big);
+		i++;
+		big++;
+	}
 	return (NULL);
-    }
-  while (*big)
-    {
-      if (ft_strnequ(little, big, ft_strlen(little)))
-	if (ft_strlen(little) + i <= len)
-	  return (big);
-      i++;
-      big++;
-    }
-  return (NULL);
 }
