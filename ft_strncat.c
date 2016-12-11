f19b6e7eff8a5f01c0469f75051ecd893cd37266
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 13:54:11 +0100

    adding ft_strncat fct

--- ft_strncat.c
+++ ft_strncat.c
@@ -1,16 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 13:53:13 by ybecret           #+#    #+#             */
+/*   Updated: 2016/12/11 13:53:36 by ybecret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strncat(char *dest, const char *src, size_t n)
 {
-  size_t dest_len;
-  size_t i;
+	size_t dest_len;
+	size_t i;
 
-  dest_len = ft_strlen(dest);
-  i = 0;
-
-  while (i++ < n && src[i++] != '\0')
-    dest[dest_len + i] = src[i];
-  dest[dest_len + i] = '\0';
-  return (dest);
+	dest_len = ft_strlen(dest);
+	i = 0;
+	while (i++ < n && src[i++] != '\0')
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
