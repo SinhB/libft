f41b5ad8bc84a3268b68cb6a6163bec9596445d7
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 13:35:11 +0100

    adding ft_memalloc fct

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -1,12 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 13:34:05 by ybecret           #+#    #+#             */
+/*   Updated: 2016/12/11 13:34:32 by ybecret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_memalloc(size_t size)
 {
-  void *alloc;
+	void *alloc;
 
-  if ((alloc = (void*)malloc(sizeof(char) * size)) == NULL)
-    return (NULL);
-  ft_bzero(alloc, size);
-  return (alloc);
+	if ((alloc = (void*)malloc(sizeof(char) * size)) == NULL)
+		return (NULL);
+	ft_bzero(alloc, size);
+	return (alloc);
 }
