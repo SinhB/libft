c7f6f714fdb7aefcb4cf35a1323b894b213b653a
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 14:37:12 +0100

    ft_lstiter ONLY header and proto

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/05 13:31:00 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/11 14:34:29 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/11 14:36:46 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,5 +90,6 @@ t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 
 #endif
