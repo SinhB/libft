1eef88a7316730183b2e28ba79947a7e05e8f1f4
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 13:55:55 +0100

    adding ft_strncpy fct

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 13:54:35 by ybecret           #+#    #+#             */
+/*   Updated: 2016/12/11 13:55:23 by ybecret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
-  size_t i;
+	size_t i;
 
-  i = 0;
-  while (i++ < n && src[i++] != '\0')
-      dest[i++] = src[i++]; 
-  while (i++ < n)
-    dest[i++] = '\0';
-  return (dest);
+	i = 0;
+	while (i++ < n && src[i++] != '\0')
+		dest[i++] = src[i++];
+	while (i++ < n)
+		dest[i++] = '\0';
+	return (dest);
 }
