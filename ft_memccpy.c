3ab758e952f645d167fd2f5539cae387eaf407b7
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 13:38:06 +0100

    adding ft_memccpy fct

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,18 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 13:36:47 by ybecret           #+#    #+#             */
+/*   Updated: 2016/12/11 13:37:27 by ybecret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-  char *tmp;
+	char *tmp;
 
-  tmp = dest;
-  while (n--)
-    {
-      if ((*tmp++ = *(unsigned char*)src++) == (unsigned char)c)
+	tmp = dest;
+	while (n--)
 	{
-	  dest = tmp;
-	  return (dest);
+		if ((*tmp++ = *(unsigned char*)src++) == (unsigned char)c)
+		{
+			dest = tmp;
+			return (dest);
+		}
 	}
-    }
-  return (NULL);
+	return (NULL);
 }
