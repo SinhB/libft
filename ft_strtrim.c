7ac7e9045e45b0b486be180bc33a88e0bd3996bb
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 15:40:39 +0100

    adding ft_strtrim fct

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 14:16:18 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/11 14:18:00 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/11 15:40:14 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,20 @@
 
 char	*ft_strtrim(char const *s)
 {
-	char	*new;
-	size_t	i;
-	size_t	j;
+	char		*new;
+	size_t		i;
+	size_t		j;
 
-	
+	i = 0;
+	while (ft_iswhitespace(s[i]))
+		i++;
+	if (!(s[i]))
+		return (ft_strnew(1));
+	j = (ft_strlen(s) - 1);
+	while (ft_iswhitespace(s[j]))
+		j--;
+	j = j - i + 1;
+	if (!(new = ft_strsub(s, i, j)))
+		return (NULL);
+	return (new);
 }
