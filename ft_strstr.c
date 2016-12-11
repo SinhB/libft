586ccd466e1a8c7caed21d6dafaa4dd7208c8e97
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 14:02:11 +0100

    adding ft_strstr fct

--- ft_strstr.c
+++ ft_strstr.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 14:01:21 by ybecret           #+#    #+#             */
+/*   Updated: 2016/12/11 14:01:34 by ybecret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strstr(const char *big, const char *little)
 {
-  return (ft_strnstr(big, little, ft_strlen(big)));
+	return (ft_strnstr(big, little, ft_strlen(big)));
 }
