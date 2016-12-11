d400d3c93eb40e7e3dca8edaa090ba8fa5f2478a
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 14:04:01 +0100

    adding ft_strsub fct

--- ft_strsub.c
+++ ft_strsub.c
@@ -1,19 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 14:02:34 by ybecret           #+#    #+#             */
+/*   Updated: 2016/12/11 14:03:34 by ybecret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strsub(char const *s, unsigned int start, size_t len)
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-  char		*new;
-  size_t	i;
+	char		*new;
+	size_t		i;
 
-  i = 0;
-  if (!(new = ft_strnew(len)))
-    return (NULL);
-  while (i < len)
-    {
-      new[i] = s[start + i];
-      i++;
-    }
-  new[i] = '\0';
-  return (new);
+	i = 0;
+	if (!(new = ft_strnew(len)))
+		return (NULL);
+	while (i < len)
+	{
+		new[i] = s[start + i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
 }
