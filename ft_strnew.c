bfa4c612d8ee7bda3b8efad6307eb01399ef44fe
Yann BECRET <ybecret@e3r5p22.42.fr>
2016-12-11 13:58:47 +0100

    adding ft_strnew fct

--- ft_strnew.c
+++ ft_strnew.c
@@ -1,12 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/11 13:57:46 by ybecret           #+#    #+#             */
+/*   Updated: 2016/12/11 13:58:19 by ybecret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strnew(size_t size)
 {
-  char *new;
+	char *new;
 
-  if ((new = (char*)malloc(sizeof(char) * (size + 1))) == NULL)
-    return (NULL);
-  ft_bzero(new, size + 1);
-  return(new);
+	if ((new = (char*)malloc(sizeof(char) * (size + 1))) == NULL)
+		return (NULL);
+	ft_bzero(new, size + 1);
+	return (new);
 }
