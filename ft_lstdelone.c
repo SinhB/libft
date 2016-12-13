88589a72c6409f36425ef3f923d2e6d3897be11f
Yann Becret <Yann@macbook-pro-de-yann.home>
2016-12-13 12:30:21 +0100

    Init

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ybecret <ybecret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 14:27:37 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/11 14:28:30 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/13 12:29:45 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	
+	(*del)((*alst)->content, (*alst)->content_size);
+		ft_memdel((void**)alst);
 }
