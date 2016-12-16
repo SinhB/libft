580ba01b4dbc7809057f86cc9a95619b6496f6aa
Yann Becret <Yann@macbook-pro-de-yann.home>
2016-12-16 11:17:51 +0100

    Init

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybecret <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ybecret <ybecret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 14:25:06 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/11 14:26:51 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/16 11:16:46 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	
+	t_list *new;
+
+	if ((new = (t_list*)malloc(sizeof(t_list))) == NULL)
+		return (NULL);
+	if (content == NULL)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+		new->next = NULL;
+		return (new);
+	}
+	if ((new->content = malloc(content_size)) == NULL)
+		return (NULL);
+	ft_memcpy(new->content, content, content_size);
+	new->content_size = content_size;
+	return (new);
 }
