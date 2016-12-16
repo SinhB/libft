e2edf8b20ee60c6978709120f9b282dd5f5e8647
Yann Becret <Yann@macbook-pro-de-yann.home>
2016-12-16 22:58:16 +0100

    Init

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: ybecret <ybecret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 13:54:35 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/12 12:39:53 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/16 22:57:42 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,15 @@ char	*ft_strncpy(char *dest, const char *src, size_t n)
 	size_t i;
 
 	i = 0;
-	while (i++ < n && src[i++] != '\0')
-		dest[i++] = src[i++];
-	while (i++ < n)
-		dest[i++] = '\0';
+	while (i < n && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
 	return (dest);
 }
