4431d30d5959138abc867c6f303a22280b629087
Yann Becret <Yann@macbook-pro-de-yann.home>
2016-12-16 22:53:19 +0100

    Init

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ybecret <ybecret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 14:10:06 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/12 12:39:48 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/16 22:52:46 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
+    size_t  i;
+    size_t  j;
+    size_t  res;
 
+    i = 0;
+    res = ft_strlen(src);
+    while(dest[i] && i < size)
+        i++;
+    j = 0;
+    if (size > 0)
+    {
+        while (src[j] && i < size - 1)
+            dest[i++] = src[j++];
+    }
+    if (j > 0)
+    {
+        dest[i] = '\0';
+        return (res + i - j);
+    }
+    return (res + i);
 }
