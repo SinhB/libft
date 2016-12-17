4cd977901c4033c97d46614a31b969fee9715a28
Yann Becret <Yann@macbook-pro-de-yann.home>
2016-12-17 13:52:31 +0100

    Init

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,13 +6,50 @@
 /*   By: ybecret <ybecret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 14:19:25 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/12 12:39:57 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/17 13:52:05 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t ft_newlen(char *s, char c)
+{
+    size_t n;
+
+    while (*s && *s == c)
+        s++;
+    n = ( *s ? 1 : 0);
+    while (*s)
+    {
+        if (*s == c && *(s + 1) != c && *(s + 1))
+            n++;
+        s++;
+    }
+    return (n);
+}
+
 char	**ft_strsplit(char const *s, char c)
 {
+    size_t  n;
+    char    *tmp;
+    char    **new;
 
+    n = ft_newlen((char*)s, c);
+    if ((new = (char**)malloc(sizeof(char*) * (n + 1))) == NULL)
+        return (NULL);
+    tmp = (char*)s;
+    while (*s)
+    {
+        if (*s == c)
+        {
+            if (tmp != s)
+                *(new++) = ft_strsub(tmp, 0, s - tmp);
+            tmp = (char*)s + 1;
+        }
+        s++;
+    }
+    if (tmp != s)
+        *(new++) = ft_strsub(tmp, 0, s - tmp);
+    *new = NULL;
+    return (new - n);
 }
