70b2ddcb0ed07f9f79b2ad9ac05bc4ad57ec0387
Yann Becret <Yann@macbook-pro-de-yann.home>
2016-12-17 14:33:23 +0100

    Init

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ybecret <ybecret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/05 16:11:03 by ybecret           #+#    #+#             */
-/*   Updated: 2016/12/17 14:28:26 by ybecret          ###   ########.fr       */
+/*   Updated: 2016/12/17 14:33:03 by ybecret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	while (s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
-			return ((unsigned char*)s1[i] - (unsigned char*)s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
